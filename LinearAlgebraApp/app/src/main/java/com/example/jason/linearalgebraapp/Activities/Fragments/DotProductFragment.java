6da8083480b6fb454ca4e20d4c2bc9c0b00913a7
max rudolph <maxrudolph1@lawn-128-61-30-153.lawn.gatech.edu>
2016-11-13 06:08:12 -0500

    Nice Hair, Jason

--- LinearAlgebraApp/app/src/main/java/com/example/jason/linearalgebraapp/Activities/Fragments/DotProductFragment.java
+++ LinearAlgebraApp/app/src/main/java/com/example/jason/linearalgebraapp/Activities/Fragments/DotProductFragment.java
@@ -2,10 +2,7 @@ package com.example.jason.linearalgebraapp.Activities.Fragments;
 
 import android.graphics.Color;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
-import android.support.v7.widget.Toolbar;
 import android.text.InputType;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -16,7 +13,6 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.Spinner;
-import android.widget.TextView;
 
 import com.example.jason.linearalgebraapp.R;
 
@@ -33,11 +29,11 @@ public class DotProductFragment extends Fragment {
     private LinearLayout vector2;
     private Spinner rows;
 
-    public static DotProductFragment newInstance(int threadIndex) {
+    public static DotProductFragment newInstance(int index) {
         DotProductFragment fragment = new DotProductFragment();
 
         Bundle arguments = new Bundle();
-        arguments.putInt(KEY_THREAD_INDEX, threadIndex);
+        arguments.putInt(KEY_THREAD_INDEX, index);
 
         fragment.setArguments(arguments);
 
@@ -112,21 +108,23 @@ public class DotProductFragment extends Fragment {
             @Override
             public void onClick(View view) {
                 int sum = 0;
+                int[] v1 = new int[vector1.getChildCount()];
+                int[] v2 = new int[vector2.getChildCount()];
                 for (int i = 0; i < vector1.getChildCount(); i++) {
                     EditText current = (EditText) vector1.getChildAt(i);
                     EditText current2 = (EditText) vector2.getChildAt(i);
-
-                    int number1;
-                    int number2;
                     try {
-                        number1 = Integer.parseInt(current.getText().toString());
-                        number2 = Integer.parseInt(current2.getText().toString());
+                        v1[i] = Integer.parseInt(current.getText().toString());
+                        v2[i] = Integer.parseInt(current2.getText().toString());
                     } catch (NumberFormatException e) {
                         return;
                     }
-                    sum += number1 * number2;
                 }
-                dotProduct.setText(String.valueOf(sum));
+                if (v1.length != v2.length) {
+                    return;
+                }
+                int ans = MatrixOperations.dotProduct(v1, v2);
+                dotProduct.setText(String.valueOf(ans));
             }
         });
 
